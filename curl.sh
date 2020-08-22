939b2d88902495349204ddb41c6e24866fe61357
jipg <jipgamez>
2020-08-22 17:04:25 -0500

    new playbook

---
+++ curl.sh
@@ -0,0 +1,2 @@
+CURL='curl -k --user USER:PASSWD -i X GET -H "Accept: application/json" http://192.168.56.104/api/v2'
+echo $(curl -k --user USER:PASSWD -i X GET -H "Accept: application/json" http://192.168.56.104/api/v2)
